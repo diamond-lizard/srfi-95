97c112bc639f135f2ecce73f542df317ff96fded
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-16 08:24:13 -0800

    Copied in from SRFI-95 Chicken 4 egg

---
+++ tests/run.scm
@@ -0,0 +1,37 @@
+(use srfi-95 test)
+
+(test-group
+ "srfi-95"
+
+ (test-assert
+  "sorted?"
+  (sorted? '(1 2 3) <))
+
+ (test
+  "sorted? with unsorted"
+  #f
+  (sorted? '(1 2 3) >))
+
+ (test-assert
+  "sorted? with key"
+  (sorted? '(#\a #\b #\c) < char->integer))
+
+ (test
+  "merge"
+  '(1 2 3 4 5 6)
+  (merge '(1 2 3) '(4 5 6) <))
+
+ (test
+  "merge!"
+  '(1 2 3 4 5 6)
+  (merge! '(1 2 3) '(4 5 6) <))
+
+ (test
+  "sort"
+  '(1 2 3)
+  (sort '(3 2 1) <))
+
+ (test
+  "sort!"
+  '(1 2 3)
+  (sort! '(3 2 1) <)))
